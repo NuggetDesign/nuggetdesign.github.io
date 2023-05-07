3aa1b0cdc079a3847dfa3de95bde64ec35c3b22e
NuggetDesign <nuggetssignature@gmail.com>
2023-05-07 22:38:13 +0700

    mobile navigation

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const textContainer = document.querySelector('.text-container');
-const textNodes = textContainer.querySelectorAll('span');
+// const textContainer = document.querySelector('.text-container');
+// const textNodes = textContainer.querySelectorAll('span');
 
-// Clone the text elements and append them to the container
-textNodes.forEach(node => textContainer.appendChild(node.cloneNode(true)));
+// // Clone the text elements and append them to the container
+// textNodes.forEach(node => textContainer.appendChild(node.cloneNode(true)));
 
-// Start the animation
-textContainer.style.animationPlayState = 'running';
+// // Start the animation
+// textContainer.style.animationPlayState = 'running';
+
+const navButton = document.getElementById('navButton');
+const menu = document.getElementById('menu');
+const links = menu.getElementsByTagName('a');
+
+navButton.addEventListener('click', function() {
+  navButton.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+Array.from(links).forEach(function(link) {
+  link.addEventListener('click', function() {
+    navButton.classList.remove('active');
+    menu.classList.remove('active');
+  });
+});
