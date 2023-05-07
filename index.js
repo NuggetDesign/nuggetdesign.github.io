4f4b5cdeca013ac2d6a6b5b6fc5f2e3d184405bf
NuggetDesign <nuggetssignature@gmail.com>
2023-05-08 01:18:52 +0700

    mobile and js fixes

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-// const textContainer = document.querySelector('.text-container');
-// const textNodes = textContainer.querySelectorAll('span');
-
-// // Clone the text elements and append them to the container
-// textNodes.forEach(node => textContainer.appendChild(node.cloneNode(true)));
-
-// // Start the animation
-// textContainer.style.animationPlayState = 'running';
-
-const navButton = document.getElementById('navButton');
-const menu = document.getElementById('menu');
-const links = menu.getElementsByTagName('a');
-
-navButton.addEventListener('click', function() {
-  navButton.classList.toggle('active');
-  menu.classList.toggle('active');
-});
-
-Array.from(links).forEach(function(link) {
-  link.addEventListener('click', function() {
-    navButton.classList.remove('active');
-    menu.classList.remove('active');
-  });
-});
+function contactUsSection() {
+    const textContainer = document.querySelector('.text-container');
+    const textNodes = textContainer.querySelectorAll('span');
+  
+    // Clone the text elements and append them to the container
+    textNodes.forEach(node => textContainer.appendChild(node.cloneNode(true)));
+  
+    // Start the animation
+    textContainer.style.animationPlayState = 'running';
+  }
+  
+  function navigationButton() {
+    const navButton = document.getElementById('navButton');
+    const menu = document.getElementById('menu');
+    const links = menu.getElementsByTagName('a');
+  
+    navButton.addEventListener('click', function() {
+      navButton.classList.toggle('active');
+      menu.classList.toggle('active');
+    });
+  
+    Array.from(links).forEach(function(link) {
+      link.addEventListener('click', function() {
+        navButton.classList.remove('active');
+        menu.classList.remove('active');
+      });
+    });
+  }
+  
+  navigationButton();
