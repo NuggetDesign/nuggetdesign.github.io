aa1bc62b949a7fa6ade3a550699dbe1a67adb138
NuggetDesign <nuggetssignature@gmail.com>
2023-05-08 21:25:32 +0700

    navigation tablet & mobile

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function navigationButton() {
 
   const tl = gsap.timeline({ paused: true });
 
-  tl.to(menu, { height: '360', duration: 0 });
+  tl.to(menu, { height: '100%', duration: 0 });
   tl.fromTo(
     menu,
     { opacity: 0, paddingTop: 0, paddingBottom: 0 },
