bb1f04615948d46d67fb60ddf2ce1411bc5253b4
NuggetDesign <nuggetssignature@gmail.com>
2023-05-08 18:04:44 +0700

    mobile version

--- index.js
+++ index.js
@@ -14,15 +14,45 @@ function navigationButton() {
   const menu = document.getElementById('menu');
   const links = menu.getElementsByTagName('a');
 
+  const tl = gsap.timeline({ paused: true });
+
+  tl.to(menu, { height: '340', duration: 0 });
+  tl.fromTo(
+    menu,
+    { opacity: 0, paddingTop: 0, paddingBottom: 0 },
+    { opacity: 1, paddingTop: '44px', paddingBottom: '16px', duration: 0.3, ease: 'power2.out' }
+  );
+  tl.fromTo(
+    menu,
+    { clipPath: 'inset(0 0 100% 0)' },
+    { clipPath: 'inset(0 0 0% 0)', duration: 0.3, ease: 'power2.out' }
+  );
+
   navButton.addEventListener('click', function() {
     navButton.classList.toggle('active');
     menu.classList.toggle('active');
+
+    if (menu.classList.contains('active')) {
+      menu.style.height = '0';
+      menu.style.visibility = 'visible';
+      menu.style.clipPath = 'inset(0 0 100% 0)';
+      tl.play();
+    } else {
+      tl.reverse().then(() => {
+        menu.style.height = '0';
+        menu.style.visibility = 'hidden';
+      });
+    }
   });
 
   Array.from(links).forEach(function(link) {
     link.addEventListener('click', function() {
       navButton.classList.remove('active');
       menu.classList.remove('active');
+      tl.reverse().then(() => {
+        menu.style.height = '0';
+        menu.style.visibility = 'hidden';
+      });
     });
   });
 }
