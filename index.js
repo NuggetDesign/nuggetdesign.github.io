e60972be7ae8b0a7abb58b571d2eb77a49c81375
NuggetDesign <nuggetssignature@gmail.com>
2023-05-08 14:21:18 +0700

    preloader

--- index.js
+++ index.js
@@ -9,22 +9,46 @@ function contactUsSection() {
     textContainer.style.animationPlayState = 'running';
 }
   
-  function navigationButton() {
-    const navButton = document.getElementById('navButton');
-    const menu = document.getElementById('menu');
-    const links = menu.getElementsByTagName('a');
-  
-    navButton.addEventListener('click', function() {
-      navButton.classList.toggle('active');
-      menu.classList.toggle('active');
+function navigationButton() {
+  const navButton = document.getElementById('navButton');
+  const menu = document.getElementById('menu');
+  const links = menu.getElementsByTagName('a');
+
+  navButton.addEventListener('click', function() {
+    navButton.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+
+  Array.from(links).forEach(function(link) {
+    link.addEventListener('click', function() {
+      navButton.classList.remove('active');
+      menu.classList.remove('active');
     });
+  });
+}
+
+navigationButton();
+
+window.addEventListener('load', function() {
+  const preloader = document.getElementById('preloader');
+  const logo = document.getElementById('logo');
+  const image = new Image();
   
-    Array.from(links).forEach(function(link) {
-      link.addEventListener('click', function() {
-        navButton.classList.remove('active');
-        menu.classList.remove('active');
-      });
-    });
+  image.onload = function() {
+    const timeline = gsap.timeline({ onComplete: removePreloader });
+    
+    timeline
+      .from(preloader, { opacity: 1, duration: 1, ease: 'power2.out' })
+      .to(preloader, { opacity: 0, duration: 0.5 }, '-=0.5')
+      .from(logo, { opacity: 0, scale: 0.5, duration: 0.5 });
+    
+    preloader.style.display = 'flex';
+  };
+  
+  image.src = 'images/logo.png';
+
+  function removePreloader() {
+    // Remove the preloader
+    preloader.remove();
   }
-  
-  navigationButton();
+});
